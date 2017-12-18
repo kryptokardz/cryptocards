f195d1c1125c7c900e524257df800f8b4d20190a
Hewlette Dibble <hcodydibble@gmail.com>
2017-12-18 11:11:13 -0800

    updated Monster model to have random values for name, health, defense, attack and id. updated the requirements.txt to include imported packages. added monsters app to settings

--- cryptomonsters/cryptomonsters/settings.py
+++ cryptomonsters/cryptomonsters/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cryptomonsters'
+    'cryptomonsters',
+    'monsters'
 ]
 
 MIDDLEWARE = [
@@ -78,9 +79,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME', ''),
-        # 'USER': os.environ.get('DB_USER', ''),
-        # 'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        # 'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': '5432',
         'TEST': {
             'NAME': os.environ.get('TEST_DB', ''),
--- cryptomonsters/monsters/models.py
+++ cryptomonsters/monsters/models.py
@@ -1,22 +1,46 @@
 """Moster model."""
 from django.contrib.auth.models import User
-
 from django.db import models
+from faker import Faker
+from random import randint, choice
+
+fake = Faker()
+
+
+def random_name():
+    return fake.first_name()
+
+
+def random_stats():
+    return randint(1, 10)
+
+
+def random_id():
+    return hash(''.join(fake.words()))
 
 
 class Monster(models.Model):
     """Create new monster model."""
+    TYPES = (
+        ('Slime'),
+        ('Skeleton'),
+        ('Zombie'),
+        ('Minotaur')
+    )
 
     user = models.ForeignKey(
         User,
         related_name='monsters',
         on_delete=models.CASCADE)
     img_file = models.ImageField(upload_to='monsters_dir')
-    name = models.CharField(max_length=30)
-    description = models.TextField(blank=True)
-    date_uploaded = models.DateTimeField(auto_now_add=True)
-    date_published = models.DateTimeField()
+    name = models.CharField(max_length=30, default=random_name)
+    health = models.IntegerField(default=random_stats)
+    defense = models.IntegerField(default=random_stats)
+    attack = models.IntegerField(default=random_stats)
+    monster_type = models.CharField(max_length=100, default=choice(TYPES))
+    unique_id = models.BigIntegerField(default=random_id)
 
     def __str__(self):
         """Render as string."""
         return self.title
+
