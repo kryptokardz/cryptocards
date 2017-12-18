cc8228fcff6685f2c89200e352fb1a0c910cdf40
chai <chaitanyanarukulla@gmail.com>
2017-12-18 15:13:53 -0800

    updated mining views

--- cryptomonsters/mining/views.py
+++ cryptomonsters/mining/views.py
@@ -5,7 +5,7 @@ from mining.scripts import blockchain
 
 
 def miningView(request):
-    """Cryptomonster home view."""
+    """."""
     # request.user.username = 'bob'
     # new_blockchain = blockchain.blockchain.new_block(request.user.username)
     return render(request, 'cryptomonsters/mining.html')
