4e53d05f1985a028bccb942db47d9e3dca7c7570
Mark Reynoso <mreynoso@spu.edu>
2017-12-18 15:11:06 -0800

    Adding admin back in

--- cryptomonsters/cryptomonsters/settings.py
+++ cryptomonsters/cryptomonsters/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'LOCALHOST']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
