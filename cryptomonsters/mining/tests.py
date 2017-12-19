09508f1b5d8220d0af748b3179921f36bbc4c3cc
chai <chaitanyanarukulla@gmail.com>
2017-12-18 18:46:55 -0800

    Added test for home page and mining page

--- cryptomonsters/mining/tests.py
+++ cryptomonsters/mining/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse_lazy
 
 # Create your tests here.
+
+
+class MainRoutingTests(TestCase):
+    """Tests for the routes in imagersite."""
+
+    def test_mining_route_has_200_response(self):
+        """Test that mining route has a 200 response code."""
+        response = self.client.get(reverse_lazy('mining'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_mining_route_has_content(self):
+        """Test that mining route has a contenton the page."""
+        response = self.client.get(reverse_lazy('mining'))
+        self.assertIn(b'Cryptomonsters', response.content)
