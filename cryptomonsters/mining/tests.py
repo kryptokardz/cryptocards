c1fe9918d43cb61cba26f6ec3b84cf1a08a9b5dd
chai <chaitanyanarukulla@gmail.com>
2017-12-20 11:58:50 -0800

    Updated to run test with Django NoseTestSuiteRunner

--- cryptomonsters/mining/tests.py
+++ cryptomonsters/mining/tests.py
@@ -1,21 +1,6 @@
 """."""
 from django.test import TestCase
-from django.urls import reverse_lazy
 from .scripts.blockchain import BlockChain
-
-
-class MainRoutingTests(TestCase):
-    """Tests for the routes in imagersite."""
-
-    def test_mining_route_has_200_response(self):
-        """Test that mining route has a 200 response code."""
-        response = self.client.get(reverse_lazy('mining'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_mining_route_has_content(self):
-        """Test that mining route has a contenton the page."""
-        response = self.client.get(reverse_lazy('mining'))
-        self.assertIn(b'Cryptomonsters', response.content)
 
 
 class BlockChainTest(TestCase):
@@ -55,5 +40,5 @@ class BlockChainTest(TestCase):
         """."""
         blockchain = BlockChain()
         blockchain.new_block('bob')
-        self.assertEqual(blockchain.chain[1].user, 'bob')
+        self.assertEqual(blockchain.chain[1].User, 'bob')
 
