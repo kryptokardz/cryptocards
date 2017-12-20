54fd2d1a8c78aff1187990adcc414f2c211b7f39
Mark Reynoso <mreynoso@spu.edu>
2017-12-20 14:52:57 -0800

    moster view errors

--- cryptomonsters/monsters/views.py
+++ cryptomonsters/monsters/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView
 
 from monsters.models import Monster
 
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+
 
 class MonstersView(ListView):
     """View all monsters."""
@@ -10,6 +12,7 @@ class MonstersView(ListView):
     model = Monster
     template_name = 'monsters/all_monsters.html'
     context_object_name = 'monsters'
+    paginate_by = 24
 
     def get_queryset(self):
         """Request users profile."""
