113e99f5f041a4ea82a0fffe1f6c416e33f2162e
jensjoh01 <johnnyj2439@gmail.com>
2017-12-21 18:00:35 -0800

    changed stuff

--- cryptomonsters/mining/tasks.py
+++ cryptomonsters/mining/tasks.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
-from mining.scripts.blockchain import BlockChain
+from mining.scripts import blockchain
 import time
 
 
 @shared_task(name='tasks.p_o_w')
 def p_o_w(previous_block, ser_user):
     """."""
-    bc = BlockChain()
+    bc = blockchain.BlockChain()
     bc._proof_of_work(previous_block, ser_user)
     return 'monster is created'
 
